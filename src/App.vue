d164d381af0b9339632b72de9325b3c748d20cb5
DENPHUM\black <niklaz.soulless@gmail.com>
2022-02-20 17:09:36 +0700

    [Update] pattarn arrow

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@ const arrowKey = reactive({
     40: "<svg xmlns='http://www.w3.org/2000/svg' class='h-36 w-36' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z'/></svg>",
 });
 
+const arrowKeyFilled = reactive({
+    37: "<svg xmlns='http://www.w3.org/2000/svg' class='h-36 w-36' viewBox='0 0 20 20' fill='currentColor'><path fill-rule='evenodd' d='M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z' clip-rule='evenodd'/></svg>",
+    38: "<svg xmlns='http://www.w3.org/2000/svg' class='h-36 w-36' viewBox='0 0 20 20' fill='currentColor'><path fill-rule='evenodd' d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z' clip'rule='evenodd'/></svg>",
+    39: "<svg xmlns='http://www.w3.org/2000/svg' class='h-36 w-36' viewBox='0 0 20 20' fill='currentColor'><path fill-rule='evenodd' d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z' clip'rule='evenodd'/></svg>",
+    40: "<svg xmlns='http://www.w3.org/2000/svg' class='h-36 w-36' viewBox='0 0 20 20' fill='currentColor'><path fill-rule='evenodd' d='M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z' clip-rule='evenodd'/></svg>",
+});
+
 const arrowKeyCode = reactive([37, 38, 39, 40]);
 const inputKey = ref([]);
 const inputSet = ref([]); // Record all of user inputs
@@ -19,7 +26,7 @@ function randomSet() {
     patternSet.value.splice(0, patternSet.value.length);
     for (let i = 0; i < 6; i++) {
         let rand = Math.floor(Math.random() * 4);
-        patternSet.value.push(arrowKey[arrowKeyCode[rand]]);
+        patternSet.value.push(arrowKeyFilled[arrowKeyCode[rand]]);
     }
 }
 
